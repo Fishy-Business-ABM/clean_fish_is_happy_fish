d1b79e40249c4f394849a0d97828db8383722529
HapeFS <hape98@hotmail.com>
2023-02-09 16:08:35 +0100

    results 2 iterations

--- model.py
+++ model.py
@@ -11,7 +11,7 @@ class Model(object):
         self.regrowing_foods = set()
 
     def add_entity(self, entity):
-        if len(self.entities) >= 200:
+        if len(self.entities) >= 100:
             return
 
         assert "pos" in entity.__dict__ and isinstance(entity.pos, tuple)
